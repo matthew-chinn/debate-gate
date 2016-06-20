5e97b106721503cfaf2f0918b3a796701f73cbce
Matthew Chinn <mattchinn96@gmail.com>
2016-06-20 11:37:14 -0700

    testing fixtures

--- test/controllers/debate_controller_test.rb
+++ test/controllers/debate_controller_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class DebateControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @controller = DebatesController.new
+  end
+
+  test "should fail create" do
+    post :create, debate: { title: "hi" }
+    assert_template :new
+  end
+
+  test "should pass create" do
+    assert_difference('Debate.count') do
+      post :create, debate: { title: "hi", description: "desc" }
+    end
+  end
 end
