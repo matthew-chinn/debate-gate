6577680181feb3f47eea7b0653be83be5afa26ff
Matthew Chinn <mattchinn96@gmail.com>
2016-06-21 12:28:35 -0700

    first/last/user names added to registration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160620211345) do
+ActiveRecord::Schema.define(version: 20160621192307) do
 
   create_table "argument_counters", force: :cascade do |t|
     t.integer "argument_id"
@@ -55,6 +55,9 @@ ActiveRecord::Schema.define(version: 20160620211345) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "username"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
