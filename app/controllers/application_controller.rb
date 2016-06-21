2ae60d7e281a74d27b77c54f74712559331fe2fe
Matthew Chinn <mattchinn96@gmail.com>
2016-06-21 12:51:17 -0700

    can edit profile

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,8 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) << :username << :first_name << :last_name
     
-    #devise_parameter_sanitizer.permit(:sign_in, keys: [:username])
-    #do after setting up, allow sign in with username
+    devise_parameter_sanitizer.for(:sign_in) << :username
+
+    devise_parameter_sanitizer.for(:account_update) << :username << :first_name << :last_name
   end
 end
