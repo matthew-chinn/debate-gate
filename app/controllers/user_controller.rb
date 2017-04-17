68ec4719ac3d2396228397296308cddbfc69b51a
Matthew Chinn <mattchinn96@gmail.com>
2017-04-17 14:55:23 -0700

    only user can view their own profile

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,10 +1,19 @@
 class UserController < ApplicationController
-  def show
-    @user = User.find(params[:id])
-    @arguments = Argument.where(creator_id: @user.id)
-    @favorites = Array.new
-    @user.favorites.each do |fav_id|
-        @favorites << Argument.find(fav_id)
+    #before_action :authenticate_user!
+
+    def show
+        @user = User.find(params[:id])
+
+        if user_signed_in? and @user.id == current_user.id
+            @arguments = Argument.where(creator_id: @user.id)
+            @favorites = Array.new
+            @user.favorites.each do |fav_id|
+                @favorites << Argument.find(fav_id)
+            end
+        else
+            flash[:alert] = "You are not permitted to view that page"
+            redirect_to root_path
+            return
+        end
     end
-  end
 end 
